8dcc63b5725aecbb01f05c0293abc56b94385c7a
Galit Oksengendler <galitox333@gmail.com>
2019-05-14 11:23:08 +0300

    Handle message from server

--- src/client/AbstractClient.java
+++ src/client/AbstractClient.java
@@ -74,23 +74,8 @@ public abstract class AbstractClient implements Runnable
   */
   private int port;
 
-// CONSTRUCTORS *****************************************************
-
-  /**
-   * Constructs the client.
-   *
-   * @param  host  the server's host name.
-   * @param  port  the port number.
-   */
-  public AbstractClient(String host, int port)
-  {
-    // Initialize variables
-    this.host = host;
-    this.port = port;
-  }
-
 // INSTANCE METHODS *************************************************
-
+  
   /**
    * Opens the connection with the server.
    * If the connection is already opened, this call has no effect.
