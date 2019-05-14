8834098772d75e49de3e077fd8f2e89edfd1204d
Shani-LevR <shani.levran@gmail.com>
2019-05-14 21:34:43 +0300

    Fixed issues of server/entities caused by commit
    include adding user name to User - deleting it from Client and Employee,
    Fixing Server and UsersDB

--- src/entity/Client.java
+++ src/entity/Client.java
@@ -9,27 +9,24 @@ public class Client extends User implements java.io.Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	// Variables
-	private final String m_userName;
 	private final long m_telephone;
 	private final int m_purchaseCount;
 	
 	/* Constructor */
 	public Client(String first, String last, String password, String username,
 			String email, Permission permission, long tel, int purchaseCount) {
-		super(first, last, email, password, permission);
-		m_userName = username;
+		super(username, first, last, email, password, permission);
 		m_telephone = tel;
 		m_purchaseCount = purchaseCount;
 	}
 	
 	/* Getters */
-	public String getUsername() { return m_userName; }
 	public long getTelephone() { return m_telephone; }
 	public int getPurchaseCount() { return m_purchaseCount; }
 
 	@Override
 	public String toString() {
-		return super.toString() + "\nUser name: " + m_userName + "\nTelephone: " + m_telephone + 
+		return super.toString() + "\nTelephone: " + m_telephone + 
 				"\nPurchase count: " + m_purchaseCount;
 	}
 }
