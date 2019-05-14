9bc8fa5175237e41c19c083e581f3fed1e50b568
Shani-LevR <shani.levran@gmail.com>
2019-05-14 21:45:53 +0300

    Fixed client exception function

--- src/server/Server.java
+++ src/server/Server.java
@@ -96,7 +96,15 @@ public class Server extends AbstractServer {
 	  synchronized protected void clientException
 	  	(ConnectionToClient client, Throwable exception) 
 	  {
-		  System.out.println(client.getName() + " has disconnected.");
+		  String msg;
+		  
+		  if(!client.getName().startsWith("Thread")){
+			  msg = client.getName() + " has disconnected.";
+		  }
+		  else {
+			  msg = "A client has disconnected.";
+		  }
+		  System.out.println(msg);
 	  }
 	  
 	  /** Private method to get current user name
