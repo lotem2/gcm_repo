acc4fd2dfd1e918ef89f31754114dd5a1e430654
Shani-LevR <shani.levran@gmail.com>
2019-06-11 21:54:25 +0300

    Add GUI to server to support initial connection between client-server
    GUI displays messages of login/logout of clients, registration of users
    to system, option to choose which port to listen to.

---
+++ src/server/MainServer.java
@@ -0,0 +1,61 @@
+package server;
+
+import java.io.IOException;
+
+import gui.MainGUI;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class MainServer extends Application {
+	// Variables
+	private static ServerController m_currentServer;
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/ServerScene.fxml"));
+		AnchorPane root = (AnchorPane)loader.load();
+		Scene scene = new Scene(root);
+		m_currentServer = (ServerController) loader.getController();
+		
+		// Set stage
+		primaryStage.setResizable(false);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("GCM Server");
+		
+		// Set listener for onClose request
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+		      public void handle(WindowEvent we) {
+		          // Close server when pressing the exit button
+		    	  try {
+			    	  m_currentServer.stopServer();		    		  
+		    	  }
+		    	  catch(Exception e) {
+		    	  }
+		      }
+		  }); 
+
+		primaryStage.show();
+	}
+	
+	/**
+	 * Start of MainServer
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void stop(){
+		try {
+			// Close server when pressing the exit button
+	    	m_currentServer.stopServer();
+		} catch (Exception e) {
+		}
+	}
+}
