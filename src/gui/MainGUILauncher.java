9fa4032b00b27160c23232dfbbaae6789586b73b
Galit Oksengendler <galitox333@gmail.com>
2019-06-18 16:32:12 +0300

    Add main launcher for server, client and test

---
+++ src/gui/MainGUILauncher.java
@@ -0,0 +1,9 @@
+package gui;
+
+import javafx.application.Application;
+
+public class MainGUILauncher {
+	public static void main(String[] args) {
+		Application.launch(MainGUI.class, args);
+	}
+}
---
+++ src/server/MainServerLauncher.java
@@ -0,0 +1,9 @@
+package server;
+
+import javafx.application.Application;
+
+public class MainServerLauncher {
+	public static void main(String[] args) {
+		Application.launch(MainServer.class, args);
+	}
+}
