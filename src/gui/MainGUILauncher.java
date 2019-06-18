9acc81d7f77cae5fe3be11ada351f78136535a47
Galit Oksengendler <galitox333@gmail.com>
2019-06-18 16:56:28 +0300

    Remove main launchers

--- src/gui/MainGUILauncher.java
+++
@@ -1,9 +0,0 @@
-package gui;
-
-import javafx.application.Application;
-
-public class MainGUILauncher {
-	public static void main(String[] args) {
-		Application.launch(MainGUI.class, args);
-	}
-}
--- src/server/MainServerLauncher.java
+++
@@ -1,9 +0,0 @@
-package server;
-
-import javafx.application.Application;
-
-public class MainServerLauncher {
-	public static void main(String[] args) {
-		Application.launch(MainServer.class, args);
-	}
-}
